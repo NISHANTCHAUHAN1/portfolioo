823f3b9896eeee0c8a804737204b9b3677cda711
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-07-13 23:47:54 -0400

    resumebtnchnage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.css'
 import port_img from '../../assets/profile_img.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import resumePdf from '../../assets/Nishantresume.pdf';
 
 const Home = () => {
   return (
@@ -11,7 +12,9 @@ const Home = () => {
        <p className='home-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi similique eum repellendus.</p>
        <div className="hero-action">
         <div className='hero-connect'><AnchorLink className='anchor-link' href='#contact'><p>Connect With Me</p></AnchorLink></div>
-        <div className="hero-resume">My Resume</div>
+        <div className="hero-resume">
+          <a href={resumePdf}>My Resume</a>
+        </div>
        </div>
     </div>
   )
